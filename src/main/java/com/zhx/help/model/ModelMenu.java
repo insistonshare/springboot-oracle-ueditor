f300a6571d6bd8102b0006da628362109373fe58
longfeizheng <longfei_zheng@163.com>
2017-03-19 22:47:57 +0800

    整合ueditor

--- src/main/java/com/zhx/help/model/ModelMenu.java
+++ src/main/java/com/zhx/help/model/ModelMenu.java
@@ -50,4 +50,43 @@ public class ModelMenu {
     public void setLevel(Integer level) {
         this.level = level;
     }
+
+    /**
+
+     insert into model_menu (ID, NAME, PARENT_ID)
+     values (2, '系统开发', 0);
+
+     insert into model_menu (ID, NAME, PARENT_ID)
+     values (3, '系统维护', 0);
+
+     insert into model_menu (ID, NAME, PARENT_ID)
+     values (4, '接口管理', 0);
+
+     insert into model_menu (ID, NAME, PARENT_ID)
+     values (5, '权限管理', 1);
+
+     insert into model_menu (ID, NAME, PARENT_ID)
+     values (6, '角色管理', 1);
+
+     insert into model_menu (ID, NAME, PARENT_ID)
+     values (7, '用户管理', 1);
+
+     insert into model_menu (ID, NAME, PARENT_ID)
+     values (8, '表单管理', 2);
+
+     insert into model_menu (ID, NAME, PARENT_ID)
+     values (9, '模块管理', 2);
+
+     insert into model_menu (ID, NAME, PARENT_ID)
+     values (10, '系统监控', 3);
+
+     insert into model_menu (ID, NAME, PARENT_ID)
+     values (11, '客户端管理', 4);
+
+     insert into model_menu (ID, NAME, PARENT_ID)
+     values (1, '系统设置', 0);
+
+
+
+     */
 }
